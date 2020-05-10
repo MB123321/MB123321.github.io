378bd3a6ad268dc58393c6f8f1590ee8d8f6a7ac
Mariella126 <59878257+Mariella126@users.noreply.github.com>
2020-05-10 10:23:12 +0200

    update

--- style.java
+++ style.java
@@ -1,9 +1,42 @@
-googleMap {
-    var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
-      };
-      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-      }
-}
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";}
 
+
+
+  $('.like-btn').on('click', function() {
+    $(this).toggleClass('is-active');
+ });
+ $('.minus-btn').on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value &amp;gt; 1) {
+      value = value - 1;
+  } else {
+      value = 0;
+  }
+
+$input.val(value);
+
+});
+
+$('.plus-btn').on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value &amp;lt; 100) {
+      value = value + 1;
+  } else {
+      value =100;
+  }
+
+  $input.val(value);
+});
